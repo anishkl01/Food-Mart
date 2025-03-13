b20b70345143502675b13619d96b6fc684ec8e94
anishkl01 <anishkulal777@gmail.com>
2025-03-14 00:16:38 +0530

    practice commit

--- Foodmart/src/main/java/com/foodmart/org/FoodmartApplication.java
+++ Foodmart/src/main/java/com/foodmart/org/FoodmartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodmartApplication {
-//d
+//dd
 	public static void main(String[] args) {
 		SpringApplication.run(FoodmartApplication.class, args);
 	}
