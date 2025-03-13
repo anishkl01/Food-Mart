c72650ebbad96a4ef0d2eb715eea1194e8244404
anishkl01 <anishkulal777@gmail.com>
2025-03-14 00:41:12 +0530

    practice3

--- Foodmart/src/main/java/com/foodmart/org/service/FoodService.java
+++ Foodmart/src/main/java/com/foodmart/org/service/FoodService.java
@@ -8,7 +8,7 @@ import com.foodmart.org.io.FoodRequest;
 import com.foodmart.org.io.FoodResponse;
 
 public interface FoodService {
-
+//dd
 	String uploadFile(MultipartFile file);
 	
 	FoodResponse addFood(FoodRequest foodRequest, MultipartFile file);
